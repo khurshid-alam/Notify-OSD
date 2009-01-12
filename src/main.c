9be58d2b9ccd44af29d5b2aafbeb107d03bd10f1
Mirco Müller <mirco.mueller@ubuntu.com>
2009-01-12 10:20:37 +0100

    fortify sanity checks

--- src/main.c
+++ src/main.c
@@ -50,7 +50,9 @@ main (int    argc,
 
 	connection = dbus_create_service_instance (DBUS_NAME);
 	if (connection == NULL)
+	{
 		g_error ("Could not register instance");
+	}
 
 	dbus_g_connection_register_g_object (connection,
 					     DBUS_PATH,
