ef60726fa6e65d6e96cc08a6474c6200e7164798
Mirco Müller <mirco.mueller@ubuntu.com>
2009-02-18 17:42:26 +0000

    changed test-script to use the new icons from the design-team, needs human-icon-theme >= 0.33

--- src/send-test-notification.sh
+++ src/send-test-notification.sh
@@ -1,25 +1,22 @@
 #!/bin/sh
 
-DIR=$HOME/devel/alsdorf
-#DIR=$HOME/src/alsdorf
-
+notify-send "WiFi signal found" -i /usr/share/icons/Human/scalable/status/notification-network-wireless-medium.svg
+sleep 2
+notify-send "WiFi signal lost" -i /usr/share/icons/Human/scalable/status/notification-network-wireless-disconnected.svg
+sleep 2
+notify-send " " -i /usr/share/icons/Human/scalable/status/notification-audio-volume-medium.svg -h int:value:75 -h string:synchronous:volume
+sleep 2
+notify-send " " -i /usr/share/icons/Human/scalable/status/notification-audio-volume-low.svg -h int:value:30 -h string:synchronous:volume
+sleep 2
+notify-send " " -i /usr/share/icons/Human/scalable/status/notification-display-brightness-high.svg -h int:value:100 -h string:synchronous:brightness
+sleep 2
+notify-send " " -i /usr/share/icons/Human/scalable/status/notification-display-brightness-medium.svg -h int:value:45 -h string:synchronous:brightness
+sleep 2
 notify-send "Testing markup" "Some <b>bold</b>, <u>underlined</u>, <i>italic</i> text"
 sleep 2
 notify-send "Jamshed Kakar" "Hey, what about this restaurant? http://www.blafasel.org
 
 Would you go from your place by train or should I pick you up from work? What do you think?"
-sleep 2
-notify-send " " -i notification-volume -h int:value:75 -h string:synchronous:volume
-sleep 2
-notify-send " " -i notification-volume -h int:value:30 -h string:synchronous:volume
-sleep 2
-notify-send " " -i notification-brightness -h int:value:100 -h string:synchronous:brightness
-sleep 2
-notify-send " " -i notification-brightness -h int:value:45 -h string:synchronous:brightness
-sleep 2
-notify-send "WiFi signal found" -i $DIR/icons/wifi-75.svg
-sleep 2
-notify-send "WiFi signal lost" -i $DIR/icons/wifi-lost.svg
 sleep 2
 notify-send "English bubble" "The quick brown fox jumps over the lazy dog." -i /usr/share/icons/Human/scalable/places/network.svg
 sleep 2
