e04d875c8f53907f349a3ba853a4433b9711eac3
Eitan Isaacson <eitan@ascender.com>
2009-02-27 22:00:53 +0200

    Remove codename.

--- src/bubble-window-accessible.c
+++ src/bubble-window-accessible.c
@@ -2,7 +2,7 @@
 **3456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789
 **      10        20        30        40        50        60        70        80
 **
-** Codename "alsdorf"
+** notify-osd
 **
 ** bubble-window-accessible.c - implements an accessible bubble window
 **
