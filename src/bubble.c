8ba3116bf413f7b3157260b758f5ba6753892211
Mirco Müller <mirco.mueller@ubuntu.com>
2009-03-27 16:20:56 +0100

    quick fix for icon-rescaling in the case when bubble is re-rendered upon change of font-face, font-size or DPI while a bubble is displayed

--- src/bubble.c
+++ src/bubble.c
@@ -2951,6 +2951,15 @@ bubble_recalc_size (Bubble *self)
 	d    = self->defaults;
 	priv = GET_PRIVATE (self);
 
+	/* FIXME: a quick fix to rescale an icon (e.g. user changed font-size or
+	** DPI while a bubble is displayed, thus bubble is re-rendered and the
+	** icon needs to adapt to the new size) */
+	priv->icon_pixbuf = gdk_pixbuf_scale_simple (
+				priv->icon_pixbuf,
+                                EM2PIXELS (defaults_get_icon_size (d), d),
+                                EM2PIXELS (defaults_get_icon_size (d), d),
+				GDK_INTERP_HYPER);
+
 	bubble_determine_layout (self);
 
 	new_bubble_width =
