8e0f6a7de9925d6d4ca128b6816ea467410f3402
Mirco Müller <mirco.mueller@canonical.com>
2013-12-04 16:43:44 +0000

    Avoid a potential leak of the source, if a bubble gets dismissed before it normally times out.

--- src/bubble.c
+++ src/bubble.c
@@ -2036,6 +2036,12 @@ bubble_dispose (GObject* gobject)
 		priv->draw_handler_id = 0;
 	}
 
+	if (priv->timer_id)
+	{
+		g_source_remove (priv->timer_id);
+		priv->timer_id = 0;
+	}
+
 	if (priv->tile_background_part)
 	{
 		tile_destroy (priv->tile_background_part);
@@ -3031,6 +3037,11 @@ gboolean
 bubble_timed_out (Bubble* self)
 {
 	g_return_val_if_fail (IS_BUBBLE (self), FALSE);
+
+	/* This function always returns FALSE, which removes the timer
+	 * source. Unset priv->timer_id so that we don't call
+	 * g_source_remove() on it later. */
+	bubble_set_timer_id (self, 0);
 
 	if (GET_PRIVATE (self)->composited)
 	{
